419fcbdef5e748b66b10ee439a5959650382ce24
ashishjha16 <ashisjha7628@gmail.com>
2026-09-20 12:24:52 +0530

    initial commit

--- oops/type_of_class/interface_classs/interfaceee.java
+++ oops/type_of_class/interface_classs/interfaceee.java
@@ -3,6 +3,12 @@ package interface_classs;
 interface car{
     void applybreaks(int decrement) ;
     void speedup(int increment);
+    default void stop (){
+        System.out.println("stoping BMW");
+    }
+    //here default is use kue kii aapn nai already class mai aagar implement kar diya ha toh 
+    //aapn saare class ko chnage kar ne nahi jaye gai naa is liye interface mai he wao line 
+    // default bolke declare kar de ne kii and us kai  argument bhi declare kar de nai kai 
 }
 class BMW implements car{
     public void applybreaks(int decrement){
@@ -17,5 +23,6 @@ public class interfaceee{
         BMW sport = new BMW();
         sport.applybreaks(40);
         sport.speedup(800);
+        sport.stop();
     } 
 }
